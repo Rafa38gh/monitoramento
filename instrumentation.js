c024f10d44c15ee40bcb4563228247c22551303d
Rafa38gh <siscaro.rafael96@gmail.com>
2025-12-11 13:53:49 -0300

    Testando o OpenTelemetry

---
+++ instrumentation.js
@@ -0,0 +1,33 @@
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-http");
+const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "monitoramento-api",
+  }),
+
+  traceExporter: new OTLPTraceExporter({
+    url: "http://otel-collector:4318/v1/traces",
+  }),
+
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: "http://otel-collector:4318/v1/metrics",
+    }),
+    exportIntervalMillis: 10000,
+  }),
+
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-http": { enabled: true },
+      "@opentelemetry/instrumentation-express": { enabled: true },
+    }),
+  ],
+});
+
+sdk.start();
